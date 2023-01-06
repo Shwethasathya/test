fbba42b78f2d572bc873db8536282b49ae4e88c9
sshwetha.xworkz@gmail.com <sshwetha.xworkz@gmail.com>
2023-01-06 13:32:13 +0530

    test

---
+++ src/com/xworkz/test/boot/Timmy.java
@@ -0,0 +1,19 @@
+package com.xworkz.test.boot;
+
+public class Timmy {
+
+	public static void main(String[] args) {
+
+		int num = 9;
+
+		if (num % 2 == 0)
+
+			System.out.println("number of flower in petal is even " + num);
+
+		else {
+			System.out.println("number of petals in flower is odd " + num);
+		}
+
+	}
+
+}
